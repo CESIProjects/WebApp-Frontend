09ec7ff46a94d4039a2bd5810687afee1f6b356c
XoforickS <alexandre77.nourry@hotmail.fr>
2023-11-15 15:23:42 +0100

    Edit and Create different color on condition, edit svg for the button | Removing package-lock for evryone

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,23 +9,25 @@
                 <li v-for="customer in customers" :key="customer.id" class="flex justify-between">
                     {{ customer.id }} - {{ customer.name }}
                     <div class="flex space-x-2">
-                        <button @click="editCustomer(customer.id)" class="bg-orange-500 p-2 rounded-lg text-white">Modifier</button>
+                        <button @click="editCustomer(customer.id)" class="bg-orange-500 p-2 rounded-lg text-white flex"><img src="/svg/edit.svg" class="text-white w-5 h-5 mr-1 mt-1" />Modifier</button>
                         <button @click="deleteCustomer(customer.id)"  class="bg-red-500 p-2 rounded-lg text-white flex"><img src="/svg/delete.svg" class="text-white w-5 h-5 mr-1 mt-1" />Supprimer</button>
                     </div>
                 </li>
             </ul>
         </div>
 
-        <div v-if="showForm" class="px-8 py-4 text-xl mx-auto w-1/4 bg-white rounded-lg">
+        <div v-if="showForm" class="p-8 text-xl mx-auto w-1/4 bg-white rounded-lg shadow-lg">
             <div class="flex justify-between">
                 <h3>{{ editMode ? 'Modifier le customer' : 'Ajouter un Customer' }}</h3>
                 <button @click="showForm = false">X</button>
             </div>
-            <form @submit.prevent="submitForm">
-                <label for="name">Nom:</label>
-                <input type="text" v-model="formData.name" required>
+            <form @submit.prevent="submitForm" class="mt-4">
+                <div class="grid gap-2">
+                    <label for="name">Nom:</label>
+                    <input type="text" class="bg-gray-100 rounded-lg focus:outline-none p-2" v-model="formData.name" required>
+                </div>
 
-                <button type="submit">{{ editMode ? 'Modifier' : 'Ajouter' }}</button>
+                <button type="submit" :class="{'bg-orange-500': editMode, 'bg-green-500': !editMode }" class="mt-4 w-full py-2 px-4 text-white rounded-lg">{{ editMode ? 'Modifier' : 'Ajouter' }}</button>
             </form>
         </div>
     </div>
