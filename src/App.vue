91c447e813b4fe6df010c63a5cdd42deb593495d
Nizaar Znagui <znagui.nizaar@outlook.fr>
2023-12-15 09:57:14 +0100

    fix: rectify div

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid grid-cols-12 gap-4">
-    <nav class="h-screen col-span-2 flex flex-col justify-between bg-main-color py-4 px-6 text-white text-xl sticky top-0">
+    <nav class="sticky top-0 flex flex-col justify-between h-screen col-span-2 px-6 py-4 text-xl text-white bg-main-color">
         <div class="flex flex-col space-y-8">
             <div class="flex">
               <img src="/Gouvernement_N.png" class="w-12 h-12 mt-1 mr-3 rounded-full" alt="">
@@ -8,27 +8,27 @@
                 (RE) SOURCES <br>RELATIONNELLES
               </div>
             </div>
-            <div class="border-t-2 border-gray-200 py-2"></div>
+            <div class="py-2 border-t-2 border-gray-200"></div>
             <div class="flex flex-col space-y-8">
               <RouterLink to="/" class="flex"><img src="/svg/home.svg" class="mr-2">Acceuil</RouterLink>
               <RouterLink to="/about" class="flex"><img src="/svg/dashboard.svg" class="mr-2">Tableau de bord</RouterLink>
-              <div class="border-t-2 border-gray-200 py-2"></div>
+              <div class="py-2 border-t-2 border-gray-200"></div>
             </div>
             <div class="flex flex-col space-y-8">
               <div class="flex"><img src="/svg/recent.svg" class="mr-2">Recent</div>
               <div class="flex"><img src="/svg/document.svg" class="mr-2">Document</div>
               <div class="flex"><img src="/svg/like.svg" class="mr-2">Mes Likes</div>
-              <div class="border-t-2 border-gray-200 py-2"></div>
+              <div class="py-2 border-t-2 border-gray-200"></div>
             </div>
-            <button class="rounded-lg bg-white font-semibold text-black py-3 text-lg">+ Ajouter une publication</button>
+            <button class="py-3 text-lg font-semibold text-black bg-white rounded-lg">+ Ajouter une publication</button>
         </div>
         <div class="flex">
           <div>
             <div v-if="user && user.name">
-              <RouterLink class="bg-gray-800 py-2 px-4 rounded-lg text-sm" to="/profile">{{ user.name }}</RouterLink>
+              <RouterLink class="px-4 py-2 text-sm bg-gray-800 rounded-lg" to="/profile">{{ user.name }}</RouterLink>
             </div>
             <div v-else>
-              <RouterLink class="bg-gray-800 py-2 px-4 rounded-lg text-sm" to="/register">Compte</RouterLink>
+              <RouterLink class="px-4 py-2 text-sm bg-gray-800 rounded-lg" to="/register">Compte</RouterLink>
             </div>
           </div>
           <button v-if="user && user.name" @click="logout" class="ml-3">
@@ -36,44 +36,44 @@
               <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
             </svg>
           </button>
+        </div>
         <div class="flex flex-col">
           <div class="flex my-5"><img src="/svg/settings.svg" class="mr-2">Parametre</div>
           
-          <div class="py-1 mb-2 flex rounded-full hover:bg-gray-800">
-              <img src="/Gouvernement_N.png" class="w-12 h-12 mt-1 mr-3 rounded-full" alt=""><div>Compte TEST<br><span class="text-gray-200 text-lg">Utilisateur</span></div>
+          <div class="flex py-1 mb-2 rounded-full hover:bg-gray-800">
+              <img src="/Gouvernement_N.png" class="w-12 h-12 mt-1 mr-3 rounded-full" alt=""><div>Compte TEST<br><span class="text-lg text-gray-200">Utilisateur</span></div>
           </div>
         </div>
-      </div>
     </nav>
     <div class="col-span-7 p-8 m-4">
       <RouterView />
     </div>
-    <div class="p-8 m-4 col-span-3 text-xl">
+    <div class="col-span-3 p-8 m-4 text-xl">
       <div class="sticky top-10">
       <div class="relative w-full">
-        <input type="text" class="w-full bg-main-color placeholder:text-black py-3 pl-10 pr-4 rounded-3xl focus:outline-none" placeholder="Rechercher">
-        <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+        <input type="text" class="w-full py-3 pl-10 pr-4 bg-main-color placeholder:text-black rounded-3xl focus:outline-none" placeholder="Rechercher">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
           <img src="/svg/search.svg" class="w-5 h-5" alt="">
         </div>
       </div>
-      <div class="rounded-3xl bg-main-color mt-4 py-4 px-6">
+      <div class="px-6 py-4 mt-4 rounded-3xl bg-main-color">
         <div class="font-bold">Catégorie :</div>
-        <ul class="flex flex-col space-y-4 my-4">
+        <ul class="flex flex-col my-4 space-y-4">
           <li>
             <div class="font-semibold">Test</div>
-            <div class="ml-4 mt-1 text-sm">Ceci décris la caté</div>
+            <div class="mt-1 ml-4 text-sm">Ceci décris la caté</div>
           </li>
-          <li class="border-t border-gray-800 pt-3">
+          <li class="pt-3 border-t border-gray-800">
             <div class="font-semibold">Test</div>
-            <div class="ml-4 mt-1 text-sm">Ceci décris la caté</div>
+            <div class="mt-1 ml-4 text-sm">Ceci décris la caté</div>
           </li>
-          <li class="border-t border-gray-800 pt-3">
+          <li class="pt-3 border-t border-gray-800">
             <div class="font-semibold">Test</div>
-            <div class="ml-4 mt-1 text-sm">Ceci décris la caté</div>
+            <div class="mt-1 ml-4 text-sm">Ceci décris la caté</div>
           </li>
-          <li class="border-t border-gray-800 pt-3">
+          <li class="pt-3 border-t border-gray-800">
             <div class="font-semibold">Test</div>
-            <div class="ml-4 mt-1 text-sm">Ceci décris la caté</div>
+            <div class="mt-1 ml-4 text-sm">Ceci décris la caté</div>
           </li>
         </ul>
       </div>
