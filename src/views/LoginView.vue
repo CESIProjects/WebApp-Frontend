787cf5cb26b4d5775ccac81285cbad41da9300d0
Nizaar Znagui <znagui.nizaar@outlook.fr>
2024-04-09 09:57:52 +0200

    feat: add post creation

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,22 +3,24 @@
     class="h-screen w-full bg-[url('https://mrwallpaper.com/images/hd/arc-de-triomphe-france-1ikitad7j4ix10a5.jpg')] bg-auto bg-no-repeat bg-center"
   >
     <div class="h-full bg-gray-800/50 p-8">
-      <div class="h-full max-w-xl mx-auto flex flex-col justify-center ">
+      <div class="h-full max-w-xl mx-auto flex flex-col justify-center">
         <div class="text-center">
-          <h2 class="mt-3 text-3xl font-extrabold text-gray-200">
-            Connexion au compte
-          </h2>
+          <h2 class="mt-3 text-3xl font-extrabold text-gray-200">Connexion au compte</h2>
 
           <p class="mt-2 text-xl">
             Or create a new account
             <RouterLink class="font-medium hover:text-blue-900" to="/register"
               >here</RouterLink
             >
+          </p>
 
-         <p class="mt-2 text-xl text-gray-200">
+          <p class="mt-2 text-xl text-gray-200">
             Ou créer un nouveau compte
-            <RouterLink class="font-medium text-red-400 hover:text-blue-500" to="/register">ici</RouterLink>
-
+            <RouterLink
+              class="font-medium text-red-400 hover:text-blue-500"
+              to="/register"
+              >ici</RouterLink
+            >
           </p>
         </div>
 
@@ -36,10 +38,7 @@
           <div class="mt-2">
             <div class="space-y-4">
               <div>
-                <label
-                  for="email"
-                  class="block text-sm font-medium text-gray-700"
-                >
+                <label for="email" class="block text-sm font-medium text-gray-700">
                   Email address
                 </label>
                 <div class="mt-1">
@@ -55,10 +54,7 @@
               </div>
 
               <div class="space-y-1">
-                <label
-                  for="password"
-                  class="block text-sm font-medium text-gray-700"
-                >
+                <label for="password" class="block text-sm font-medium text-gray-700">
                   Password
                 </label>
                 <div class="mt-1">
@@ -74,9 +70,11 @@
               </div>
 
               <div>
-
-                <button type="submit" class="w-full bg-gradient-to-r from-blue-700 via-white to-red-700 flex justify-center border-gray-900 py-2 px-4 rounded-md border-4 text-lg text-gray-800 font-bold " @click="login()">
-
+                <button
+                  type="submit"
+                  class="w-full bg-gradient-to-r from-blue-700 via-white to-red-700 flex justify-center border-gray-900 py-2 px-4 rounded-md border-4 text-lg text-gray-800 font-bold"
+                  @click="login()"
+                >
                   Login
                 </button>
               </div>
