aeebba1c16aca2605daa0394f29d9d24c6a38aec
Thibaut <tmilville.pro@gmail.com>
2024-06-13 09:28:36 +0200

    Config

--- components/SidebarLeft.vue
+++ components/SidebarLeft.vue
@@ -31,12 +31,12 @@
                 d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
               />
             </svg>
-            Acceuil
+            Accueil
           </NuxtLink>
         </div>
         <div class="flex flex-col mb-5 mt-6 text-lg">
           <NuxtLink
-            to="/post"
+            to="/posts"
             class="group flex items-center justify-between gap-x-2 bg-blue-400 text-blue-900 border-blue-300 hover:bg-gray-900 hover:text-blue-400 font-semibold p-3"
           >
             <svg
