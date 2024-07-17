2f3ff5b8606e03be4b693df58abb0472332a820f
Thibault <muelthibault@gmail.com>
2024-07-17 14:29:16 +0200

    creation d'une categorie fonctionne

--- pages/index.vue
+++ pages/index.vue
@@ -38,6 +38,52 @@
         </div>
       </div>
     </div>
+
+    <div class="">
+        <div
+          v-if="modalCategory"
+          class="fixed left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover"
+          id="modal-id"
+        >
+          <div class="absolute bg-black opacity-80 inset-0 z-0"></div>
+          <div
+            class="w-full max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg bg-white"
+          >
+            <button
+                class="absolute top-3 right-3 text-white text-xl z-50"
+                @click="modalCategory = !modalCategory"
+            >
+                <Icon name="ep:close-bold" color="black" />
+            </button>
+            <form
+              @submit.prevent="createCategory"
+              class="flex flex-col space-y-4"
+            >
+              <div class="text-xl">Création d'une catégorie :</div>
+              <input
+                type="text"
+                v-model="newCategoryName"
+                placeholder="Entrer une catégorie"
+                class="bg-gray-200 py-2 px-4 focus:outline-none"
+              />
+              <div class="flex justify-end space-x-4">
+                <button
+                  class="bg-gray-500 py-2 px-4 font-semibold text-white"
+                  @click="modalCategory = !modalCategory"
+                >
+                  Annuler
+                </button>
+                <button
+                  type="submit"
+                  class="bg-green-500 py-2 px-4 font-semibold text-white"
+                >
+                  Ajouter
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     <!-- Navbar -->
     <div class="flex items-center border-b-2 border-gray-800 py-4 bg-gray-700 h-24">
       <div class="h-[2px] bg-gray-200 w-12"></div>
@@ -47,6 +93,17 @@
         <input placeholder="Rechercher par nom" class="border-2 border-gray-200 rounded-md px-4 py-3 bg-white " />
       </div>
       <div class="h-[2px] bg-gray-200 w-full"></div>
+
+      <div @click="modalCategory = true"
+        class="w-full max-w-72 mr-6 flex items-center hover:scale-105 hover:bg-gray-900 text-center transition border-2 px-4 py-3 text-gray-300 rounded-lg cursor-pointer bg-gray-800">
+        <div class="text-lg text-left font-normal border-gray-200 w-full h-full mr-2">
+            Creation d'une categorie
+        </div>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+          </svg>
+
+      </div>
 
       <div @click="dialogVisible = true"
         class="w-full max-w-72 flex items-center hover:scale-105 hover:bg-gray-900 text-center transition border-2 px-4 py-3 text-gray-300 rounded-lg cursor-pointer bg-gray-800">
@@ -85,16 +142,6 @@
             {{ post.content }}
           </div>
           <div class="py-1 mb-2 flex justify-between">
-            <div
-              class="flex items-center gap-x-2 group text-gray-800 text-xl"
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" class="w-12 h-12 bg-gray-500  p-2 rounded-full"><path fill="white" d="m14.856 15.5l1.5-1.5H6.252a2.25 2.25 0 0 0-2.25 2.25v.577c0 .892.32 1.756.9 2.435c1.395 1.634 3.442 2.531 6.097 2.707q-.007-.274.064-.561l.23-.924c-2.373-.12-4.108-.858-5.251-2.196a2.25 2.25 0 0 1-.54-1.461v-.578a.75.75 0 0 1 .75-.749zM12 2.005a5 5 0 1 1 0 10a5 5 0 0 1 0-10m0 1.5a3.5 3.5 0 1 0 0 7a3.5 3.5 0 0 0 0-7m7.1 9.165l-5.903 5.902a2.7 2.7 0 0 0-.706 1.247l-.458 1.831a1.087 1.087 0 0 0 1.319 1.318l1.83-.457a2.7 2.7 0 0 0 1.248-.707l5.902-5.902A2.286 2.286 0 0 0 19.1 12.67"/></svg>            
-                <NuxtLink class="group-hover:underline capitalize font-bold" :to="'/profile/' + post.userId">
-                  <span v-if="post.username !== userStore.user.username">{{post.username}}</span>
-                  <span v-else>Moi</span>
-
-                </NuxtLink>
-            </div>
             <div class="mr-2 group">
             </div>
           </div>
@@ -119,7 +166,9 @@ export default {
         description: "",
         category: "",
       },
-      dialogVisible: false
+      dialogVisible: false,
+      newCategoryName: '',
+      modalCategory: false
     };
   },
   mounted() {
@@ -143,6 +192,26 @@ export default {
           console.error('Error fetching categories:', error)
         })
     },
+    createCategory() {
+    const newCategory = {
+      name: this.newCategoryName,
+    };
+
+    axios
+      .post('http://localhost:8080/api/categories/post', newCategory)
+      .then((response) => {
+        console.log('Category created successfully:', response.data);
+        this.fetchCategories();
+        this.modalCategory = false;
+        this.newCategoryName = '';
+      })
+      .catch((error) => {
+        console.error('Error creating category:', error.response.data);
+        // Ajoutez ici une gestion spécifique des erreurs pour afficher à l'utilisateur
+        // ou pour déboguer plus en détail.
+      });
+  },
+
     fetchPosts() {
       axios
         .get("http://localhost:8080/api/posts")
